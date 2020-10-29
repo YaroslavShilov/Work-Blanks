e55dab1137b4259b3250169589408e0a15a62f4d
Yaroslav Shilov <workyarik@gmail.com>
2020-10-29 23:51:42 +0300

    add gulp 4 for EmailProject

--- EmailProject/gulpfile.js
+++ EmailProject/gulpfile.js
@@ -1,23 +1,18 @@
-var gulp       = require('gulp'),
+const {src, dest, parallel, series, watch, task} = require('gulp')
+
+const
 	sass         = require('gulp-sass'),
 	csso         = require('gulp-csso'),
 	cssnano      = require('gulp-cssnano'),
 	browserSync  = require('browser-sync'),
 	del          = require('del'),
-	duration     = require('gulp-duration'),
-	imagemin     = require('gulp-imagemin'),
-	imageminJR 	 = require('imagemin-jpeg-recompress'),
-	pngquant     = require('imagemin-pngquant'),
-	newer     	 = require('gulp-newer'),
-	cache        = require('gulp-cache'),
 	autoprefixer = require('gulp-autoprefixer'),
 	pug					 = require('gulp-pug'),
-	gulpSync 		 = require('gulp-sync')(gulp).sync,
 	inky 				 = require('inky'),
 	inlineCss		 = require('gulp-inline-css'),
 	inlineSource = require('gulp-inline-source');
 
-gulp.task('browser-sync', function() {
+task('browser-sync', function() {
 	browserSync({
 		server: {
 			baseDir: 'app'
@@ -26,86 +21,58 @@ gulp.task('browser-sync', function() {
 	});
 });
 
-gulp.task('sassMain', function(){
-	return gulp.src('app/sass/main.+(scss|sass)')
+task('sass', function(){
+	return src('app/sass/main.+(scss|sass)')
 		.pipe(sass())
 		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 10'], { cascade: true }))
 		.pipe(csso())
 		.pipe(cssnano())
-		.pipe(gulp.dest('app/css'))
+		.pipe(dest('app/css'))
 		.pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('inky', ['sassMain'], function() {
-	return gulp.src('app/*.html')
+task('inky', function() {
+	return src('app/*.html')
 		.pipe(inky())
-		.pipe(gulp.dest('app/'))
+		.pipe(dest('app/'))
 });
 
-gulp.task('pug', function() {
-	gulp.src('app/pug/pages/**/*.pug')
+task('pug', function() {
+	return src('app/pug/pages/**/*.pug')
 		.pipe(pug({
 			pretty: true,
 		}))
-		.pipe(gulp.dest('app/'));
-	gulp.src('app/pug/**/*.pug')
+		.pipe(dest('app/'))
 		.pipe(browserSync.stream())
 });
 
 
 
-gulp.task('watch', ['browser-sync'], function() {
+task('watch', function() {
 
-	gulp.watch('app/sass/**/*.+(scss|sass)', ['sassMain']);
-	gulp.watch('app/pug/**/*.pug', gulpSync(['pug', 'inky']));
+	watch('app/sass/**/*.+(scss|sass)', series('sass'));
+	watch('app/pug/**/*.pug', series('pug', 'inky'));
 
 });
 
 
 
-gulp.task('build', function() {
+task('build', function() {
 
-	del.sync(['dist/*', '!dist/img']);
+	del.sync(['dist/*']);
 
-	var buildInky = gulp.src('app/*.html')
+	let buildInky = src('app/*.html')
 		.pipe(inlineSource())
 		.pipe(inlineCss({
 			preserveMediaQueries: true,
 			removeLinkTags: true
 		}))
-		.pipe(gulp.dest('dist'));
+		.pipe(dest('dist'));
 
-	var imgDist = 'dist/img';
-	var builImg = gulp.src('app/img/**/*')
-		.pipe(newer(imgDist))
-		.pipe(imagemin([
-			imagemin.gifsicle({interlaced: true}),
-			imagemin.jpegtran({progressive: true}),
-			imageminJR({loops: 1, quality: "low"}),
-			imagemin.svgo(),
-			imagemin.optipng({optimizationLevel: 5}),
-			pngquant({quality: "88-100", speed: 5})
-		]))
-		.pipe(duration('gulp build'))
-		.pipe(gulp.dest(imgDist));
+	let builImg = src('app/img/**/*')
+		.pipe(dest('dist/img'));
 
 });
 
 
-//BEGIN gulp img
-gulp.task('img', function() {
-	var builImg = gulp.src('app/img/**/*')
-		.pipe(imagemin([
-			imagemin.gifsicle({interlaced: true}),
-			imagemin.jpegtran({progressive: true}),
-			imageminJR({loops: 1, quality: "low"}),
-			imagemin.svgo(),
-			imagemin.optipng({optimizationLevel: 5}),
-			pngquant({quality: "88-100", speed: 5})
-		]))
-		.pipe(duration('gulp img'))
-		.pipe(gulp.dest('dist/img'));
-});
-//END gulp img
-
-gulp.task('default', ['watch']);
+task('default', parallel('watch', 'browser-sync'));
